6e9b7f8de7a4e55c0ffec9bce1f1287befcb277d
2dpi <dwight@2dpi.co.za>
2015-06-18 19:20:23 +0200

    Update index.php

--- joomla/layout/index.php
+++ joomla/layout/index.php
@@ -1,1 +1,19 @@
-
+---
+layout: default
+title: WebMaster Pages
+overview: true
+keywords: css3, html5, responsive, grid, scalable
+---
+<div class="container_body_wrapper">
+  <div class="container_content">
+    <div class="container_article">
+      <article>
+        <h1>Layout Files</h1>
+        <p>https://docs.joomla.org/J3.x:JLayout_Improvements_for_Joomla!</p>
+      </article>
+    </div>
+    <aside class="container_aside">
+      [add nav/banners here]
+    </aside>
+  </div>
+</div>
