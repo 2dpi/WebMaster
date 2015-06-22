ece5aee3ebf59db4630ea88d5e7759227049a1c1
2dpi <dwight@2dpi.co.za>
2015-06-22 05:59:35 +0200

    Update index.php

--- joomla/ajax/index.php
+++ joomla/ajax/index.php
@@ -1,1 +1,20 @@
-xx
+---
+layout: default
+title: WebMaster Pages
+overview: true
+keywords: css3, html5, responsive, grid, scalable
+---
+<div class="container_body_wrapper">
+  <div class="container_content">
+    <div class="container_article">
+      <article>
+        <h1>Using Joomla Ajax Interface</h1>
+        <p><a href="https://docs.joomla.org/Using_Joomla_Ajax_Interface">Docs</a>.</p>
+
+      </article>
+    </div>
+    <aside class="container_aside">
+      [add nav/banners here]
+    </aside>
+  </div>
+</div>
